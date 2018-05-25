f0822714da9bd62d6add0c4cdf40cfb425f6766b
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2018-05-25 19:20:20 -0300

    improvement

--- rifaqui/core/admin.py
+++ rifaqui/core/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.admin import SimpleListFilter
+
 from .models import *
 
 
@@ -25,3 +27,24 @@ class RaffleModelAdmin(admin.ModelAdmin):
         extra = 0
         autocomplete_fields = ('client',)
     inlines = [TicketInlineModelAdmin]
+
+
+@admin.register(Ticket)
+class TicketModelAdmin(admin.ModelAdmin):
+    class RaffleFilter(SimpleListFilter):
+        title = Raffle._meta.verbose_name
+        parameter_name = 'raffle'
+
+        def lookups(self, request, model_admin):
+            raffles = set([r for r in Raffle.objects.filter(draw_date__isnull=True)])
+            return [(r.id, r.description) for r in raffles]
+
+        def queryset(self, request, queryset):
+            if self.value():
+                return queryset.filter(raffle__id__exact=self.value())
+            else:
+                messages.add_message(request, messages.WARNING, 'Escolha uma '+self.title)
+                return queryset.filter(raffle__id__exact=0)
+
+    list_display = ('raffle', 'client', 'booking_date', 'is_paid', 'number')
+    list_filter = (RaffleFilter, 'is_paid')
